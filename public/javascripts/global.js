ebbf184a2f00ad3e0c367c01647af4c7951c3978
Thomas Cho <s.thomas.cho@gmail.com>
2016-09-01 11:48:00 -0700

    Populate user info

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,13 +1,11 @@
 // Userlist data array for filling in info box
 var userListData = [];
 
-// DOM Ready ======================================
-// $(document).ready(function() { // why?
-	// Populate the user table on initial page load
+// Populate the user table on initial page load
+populateTable2();
 
-	populateTable2();
-
-// });
+// Username link click
+$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 
 // Functions ========================================
 
@@ -23,11 +21,7 @@ function populateTable2() {
 // Fill table with user data
 function populateTable() {
 	// Empty content string
-	
-
 	// jQuery AJAX call for userlist JSON
-	
-	// jQuery AJAX call for JSON
     $.getJSON('/users/userlist')
     	.then(function( data ) {
         // For each item in our JSON, add a table row and cells to the content string
@@ -40,6 +34,7 @@ function populateTable() {
 
 
 var writeHTMLTable = (data) => {
+				userListData = data;
 				var tableContent = '';
 
         $.each(data, function(){
@@ -48,9 +43,31 @@ var writeHTMLTable = (data) => {
             tableContent += '<td>' + this.email + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
-        }
-       )
+        });
 
         // Inject the whole content string into our existing HTML table
         $('#userList table tbody').html(tableContent);
-}
+};
+
+// Show User info
+function showUserInfo(event) {
+
+	//Prevent Link from Firing
+	event.preventDefault();
+
+	// Retrieve username from link rel attribute
+	var thisUserName = $(this).attr('rel');
+
+	// Get Index of object based on id value
+  var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+  // Get our User Object
+  var thisUserObject = userListData[arrayPosition];
+
+  //Populate Info Box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+
+};
