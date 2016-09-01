0d1e2b5d216b9149290a8e431cd5adb35b4f63e1
Thomas Cho <s.thomas.cho@gmail.com>
2016-09-01 12:16:18 -0700

    Add user on homepage

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,24 @@ var router = express.Router();
  * GET userlist.
  */
 router.get('/userlist', function(req, res) {
-    var db = req.db;
-		
+    var db = req.db;		
     var collection = db.get('userlist');
     collection.find({},{}).then(docs => {
         res.json(docs);
     })
 	});
+
+/* 
+ * POST to adduser
+ */
+router.post('/adduser', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.insert(req.body, function(err, result){
+		res.send(
+			(err === null) ? { msg: ''} : { msg: err}
+		);
+	});
+});
 
 module.exports = router;
